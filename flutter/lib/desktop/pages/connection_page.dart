f8a608c0e606b8f24739f5d36d377f2bc0b8a048
primmer-ocarina <primmer.08-ocarina@icloud.com>
2025-03-05 10:18:46 +0100

    Update connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -92,7 +92,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
                       children: [
                         Flexible(
                           child: Text(
-                            translate('setup_server_tip'),
+                            'eolianCloud',
                             style: TextStyle(
                                 decoration: TextDecoration.underline,
                                 fontSize: em),
